f5e021dbc4198d0a021a7c1b9776da2eff626487
srushtikanade <63073854+srushtikanade@users.noreply.github.com>
2020-06-20 00:00:10 +0530

    Create less mario.py

---
+++ pset6/less mario.py
@@ -0,0 +1,14 @@
+from cs50 import get_int
+
+while True:
+        h = get_int("enter a positive integer for height:\n")
+        if h > 0 and h<9  :
+            break
+
+for i in range(1,h+1):
+    nhash=i
+    nspace=h-i
+    print(" "*nspace,end="")
+    print("#"*nhash,end="")
+
+    print()
